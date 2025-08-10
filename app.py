42e2faf3c6cc26379574235d6ed0ca878f0f930f
adityashri01 <luckyadityashri01@gmail.com>
2025-08-10 18:12:56 +0530

    intialize deployment

--- app.py
+++ app.py
@@ -557,4 +557,5 @@ def delete_account():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 10000))
+    socketio.run(app, host='0.0.0.0', port=port)
